b06052a7df042c7f9313a1407d0cb1dd478b7f81
지상은 <sangeun@jisang-eun-ui-MacBookAir.local>
2022-04-24 23:41:49 +0900

    2022.04.24

---
+++ hello-spring/src/main/java/hello/hellospring/sevice/MemberService.java
@@ -0,0 +1,43 @@
+package hello.hellospring.sevice;
+
+import hello.hellospring.domain.Member;
+import hello.hellospring.repository.MemberRepository;
+import hello.hellospring.repository.MemoryMemberRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+public class MemberService {
+
+    public MemberService(MemberRepository memberRepository) {
+        this.memberRepository = memberRepository;
+    }
+
+    private final MemberRepository memberRepository;
+
+
+    //회원가입
+    public Long join(Member member){
+        // 같은 이름을 가진 사람은 회원가입 불가
+        validateDuplicateMember(member);
+
+        memberRepository.save(member);
+        return member.getId();
+    }
+
+    private void validateDuplicateMember(Member member) {
+        memberRepository.findByNane(member.getName())
+                .ifPresent(m -> {
+                    throw new IllegalStateException("이미 존재하는 회원입니다.");
+                });
+    }
+
+    //전체회원조회
+    public List<Member> findMembers(){
+        return memberRepository.findAll();
+    }
+
+    public Optional<Member> findOne(Long memberId){
+        return memberRepository.findById(memberId);
+    }
+}
