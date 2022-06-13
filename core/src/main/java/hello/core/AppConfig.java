414cc72a67178d6b4e092cec1469cb9d24a7ad66
지상은 <wltkddms9909@naver.com>
2022-06-13 16:05:40 +0900

    @Configuration에 대하여

--- core/src/main/java/hello/core/AppConfig.java
+++ core/src/main/java/hello/core/AppConfig.java
@@ -11,16 +11,34 @@ import hello.core.order.OrderServiceImpl;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-@Configuration
+@Configuration //빼면 돌아가긴 하지만, CGLIB 적용 안 됨 == 싱글톤 유지 불가
 public class AppConfig {
+
+    //@Bean memberService -> new MemoryMemberRepository()
+    //@Bean orderSerivice -> new MemoryMemberRepository()
+    // => 싱글톤 유지되는 것?
+
+    //예상
+    //call AppConfig.memberService
+    //call AppConfig.memberRepository
+    //call AppConfig.memberRepository
+    //call AppConfig.orderService
+    //call AppConfig.memberRepository
+
+    //실제
+    //call AppConfig.memberService
+    //call AppConfig.memberRepository => 여러번 호출되지 않음 ==> 싱글톤 보장!!!!
+    //call AppConfig.orderService
 
     @Bean
     public MemberService memberService(){
+        System.out.println("call AppConfig.memberService");
         return new MemberServiceImpl(memberRepository()); // 생성자 주입
     }
 
     @Bean
     public MemoryMemberRepository memberRepository() {
+        System.out.println("call AppConfig.memberRepository");
         return new MemoryMemberRepository();
     }
 
@@ -31,6 +49,7 @@ public class AppConfig {
 
     @Bean
     public OrderService orderService(){
+        System.out.println("call AppConfig.orderService");
         return new OrderServiceImpl(memberRepository(), discountPolicy());
     }
 
