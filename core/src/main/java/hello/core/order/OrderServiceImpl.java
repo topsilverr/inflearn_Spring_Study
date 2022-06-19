8c406a3a4fc8b65ec7daa120085b7b9ec859d6cb
지상은 <wltkddms9909@naver.com>
2022-06-20 00:10:31 +0900

    의존관계 주입

--- core/src/main/java/hello/core/order/OrderServiceImpl.java
+++ core/src/main/java/hello/core/order/OrderServiceImpl.java
@@ -12,13 +12,12 @@ public class OrderServiceImpl implements OrderService{
 
 
 
-    private final MemberRepository memberRepository; //불변
-    private DiscountPolicy discountPolicy; //불변
-    // private final DiscountPolicy discountPolicy = new FixDiscountPolicy();
+    private final MemberRepository memberRepository; //불변,필수
+    private final DiscountPolicy discountPolicy; //불변,필수수    // private final DiscountPolicy discountPolicy = new FixDiscountPolicy();
     //private final DiscountPolicy discountPolicy = new RateDiscountPolicy(); // 이렇게 바꾸는 순간 ocp(개방-폐쇄 원칙) 위반 * ocp: 확장에는 열려있으나 변경에는 닫혀있어야 함.
 
 
-    @Autowired
+    @Autowired // 생성자가 단 1개 있는 경우는 생략가능, but, 두개 이상일 경우 생략 불가능 !
     public OrderServiceImpl(MemberRepository memberRepository, DiscountPolicy discountPolicy) {
         this.memberRepository = memberRepository;
         this.discountPolicy = discountPolicy;
