71224894d89c6877e4c4fae51b0522e79062f068
지상은 <wltkddms9909@naver.com>
2022-09-16 01:49:04 +0900

    자동주입

--- core/src/main/java/hello/core/order/OrderServiceImpl.java
+++ core/src/main/java/hello/core/order/OrderServiceImpl.java
@@ -1,5 +1,6 @@
 package hello.core.order;
 
+import hello.core.annotation.MainDiscountPolicy;
 import hello.core.discount.DiscountPolicy;
 import hello.core.member.Member;
 import hello.core.member.MemberRepository;
@@ -9,7 +10,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
-@RequiredArgsConstructor //final이 붙은 변수들을 가지고 생성자를 만들어줌 즉 OrderServiceImpl 생성자 자동 생성
+//@RequiredArgsConstructor //final이 붙은 변수들을 가지고 생성자를 만들어줌 즉 OrderServiceImpl 생성자 자동 생성
 public class OrderServiceImpl implements OrderService{
 
 
@@ -19,11 +20,12 @@ public class OrderServiceImpl implements OrderService{
     //private final DiscountPolicy discountPolicy = new RateDiscountPolicy(); // 이렇게 바꾸는 순간 ocp(개방-폐쇄 원칙) 위반 * ocp: 확장에는 열려있으나 변경에는 닫혀있어야 함.
 
 
-//    @Autowired // 생성자가 단 1개 있는 경우는 생략가능, but, 두개 이상일 경우 생략 불가능 !
-//    public OrderServiceImpl(MemberRepository memberRepository, DiscountPolicy discountPolicy) {
-//        this.memberRepository = memberRepository;
-//        this.discountPolicy = discountPolicy;
-//    } -> Lombok 의 RequiredArgsConstructor 을 이용하면 필요없어지는 코드
+    @Autowired // 생성자가 단 1개 있는 경우는 생략가능, but, 두개 이상일 경우 생략 불가능 !
+    public OrderServiceImpl(MemberRepository memberRepository, @MainDiscountPolicy DiscountPolicy discountPolicy) {
+        this.memberRepository = memberRepository;
+        this.discountPolicy = discountPolicy;
+    }
+    //-> Lombok 의 RequiredArgsConstructor 을 이용하면 필요없어지는 코드
 
     @Override
     public Order createOrder(Long memberId, String itemName, int itemPrice) {
