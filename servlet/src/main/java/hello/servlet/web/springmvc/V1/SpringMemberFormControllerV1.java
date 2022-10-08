8be9540ecfb4550e3f568b80de510b860732c4fd
topsilverr <wltkddms9909@naver.com>
2022-10-08 20:19:49 +0900

    springMVC new-form 주석 수정

--- servlet/src/main/java/hello/servlet/web/springmvc/V1/SpringMemberFormControllerV1.java
+++ servlet/src/main/java/hello/servlet/web/springmvc/V1/SpringMemberFormControllerV1.java
@@ -1,5 +1,6 @@
 package hello.servlet.web.springmvc.V1;
 
+import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
@@ -7,6 +8,9 @@ import org.springframework.web.servlet.ModelAndView;
 @Controller
 // 스프링이 자동으로 스프링 빈으로 등록 -> controller 애노테이션 내부에 component 애노테이션이 들어있어서 컴포넌트 스캔 대상이 됨
 // 스프링 MVC 에서 애노테이션 기반 컨트롤러로 인식 == RequestMappingHandlerMapping 이 매핑 정보로 인식할 수 있음
+//@Component
+//@RequestMapping
+// @Controller 대신 위의 두개 애노테이션을 사용해도 됨, 단 항상 두개를 같이 써줘야함
 public class SpringMemberFormControllerV1 {
 
     @RequestMapping("/springmvc/v1/members/new-form")
