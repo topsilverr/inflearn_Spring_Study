44a057b59347b4cf292b0878c9de3822ed9dbecc
topsilverr <wltkddms9909@naver.com>
2022-10-14 01:43:52 +0900

    [fix]요청 API 설정 @RequestMapping 추가

--- springmvc/src/main/java/hello/springmvc/basic/requestmapping/MappingClassController.java
+++ springmvc/src/main/java/hello/springmvc/basic/requestmapping/MappingClassController.java
@@ -3,6 +3,7 @@ package hello.springmvc.basic.requestmapping;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
+@RequestMapping("/mapping/users")
 public class MappingClassController {
 
     /**
@@ -13,27 +14,27 @@ public class MappingClassController {
      * 회원 삭제: DELETE /users/{userId}
      */
 
-    @GetMapping("/mapping/users")
+    @GetMapping
     public String user(){
         return "get users";
     }
 
-    @PostMapping("/mapping/users")
+    @PostMapping
     public String addUser(){
         return "post users";
     }
 
-    @GetMapping("/mapping/users/{userId}")
+    @GetMapping("{userId}")
     public String findUser(@PathVariable String userId){
         return "get userId = "+userId;
     }
 
-    @PatchMapping("/mapping/users/{userId}")
+    @PatchMapping("{userId}")
     public String updateUser(@PathVariable String userId){
         return "update userId = "+ userId;
     }
 
-    @DeleteMapping("/mapping/users/{userId}")
+    @DeleteMapping("{userId}")
     public String deleteUser(@PathVariable String userId){
         return "delete userId = "+ userId;
     }
