7db474cd275b850cbf039e991ef51f96032baf4a
topsilverr <wltkddms9909@naver.com>
2022-10-15 15:12:39 +0900

    [feat] 요청파라미터 조회 V2

--- springmvc/src/main/java/hello/springmvc/basic/request/RequestParamController.java
+++ springmvc/src/main/java/hello/springmvc/basic/request/RequestParamController.java
@@ -3,6 +3,8 @@ package hello.springmvc.basic.request;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -19,5 +21,16 @@ public class RequestParamController {
         log.info("username={} , age={}",username,age);
 
         response.getWriter().write("ok");
+    }
+
+    @ResponseBody // 반환되는 값이 view 이름으로 인식되어 view 조회를 하는 것이 아닌, 반환되는 값을 http 메세지 바디에 넣어서 반환
+                  // -> @RestController 와 같은 기능
+    @RequestMapping("/request-param-v2")
+    public String requestParamV2(
+            @RequestParam("username") String memberName,
+            @RequestParam("age") int memberAge){
+
+        log.info("username={}, age={}",memberName,memberAge);
+        return "ok";
     }
 }
