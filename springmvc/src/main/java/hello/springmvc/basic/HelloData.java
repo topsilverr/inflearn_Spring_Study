db134e487164dd14fd5a579b3329e775e357c9ec
topsilverr <wltkddms9909@naver.com>
2022-10-15 16:29:07 +0900

    [feat] 요청 파라미터 조회 modelAttribute

--- springmvc/src/main/java/hello/springmvc/basic/HelloData.java
+++ springmvc/src/main/java/hello/springmvc/basic/HelloData.java
@@ -5,6 +5,6 @@ import lombok.Data;
 @Data
 // @Getter @Setter @ToString @EqualAndHashCode @RequiredArgsConstructor 를 자동 적
 public class HelloData {
-    private String userName;
+    private String username;
     private int age;
 }
