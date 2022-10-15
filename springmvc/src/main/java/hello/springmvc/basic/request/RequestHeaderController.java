6d090831457557276ddb3792c8ad22d88f10db04
topsilverr <wltkddms9909@naver.com>
2022-10-15 14:45:25 +0900

    [del] 필요없는 라이브러리 삭제

--- springmvc/src/main/java/hello/springmvc/basic/request/RequestHeaderController.java
+++ springmvc/src/main/java/hello/springmvc/basic/request/RequestHeaderController.java
@@ -1,7 +1,6 @@
 package hello.springmvc.basic.request;
 
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.cglib.core.Local;
 import org.springframework.http.HttpMethod;
 import org.springframework.util.MultiValueMap;
 import org.springframework.web.bind.annotation.CookieValue;
