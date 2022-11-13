a3f44e1e349ba0044e6887f00ff2d0c436a2a9df
topsilverr <wltkddms9909@naver.com>
2022-11-13 21:26:28 +0900

    addForm @ModelAttribute 사용으로 변경

--- item-service/src/main/java/hello/itemservice/web/basic/basicItemController.java
+++ item-service/src/main/java/hello/itemservice/web/basic/basicItemController.java
@@ -55,7 +55,7 @@ public class basicItemController {
         return "basic/item";
     }
 
-    @PostMapping("/add")
+    //@PostMapping("/add")
     // @ModelAttribute : model 객체 생성해주기 & 괄호 안에 있는 이름으로 모델에 넣어주는 두 가지 역할을 행함
     public String addItemV2(@ModelAttribute("item") Item item, Model model)
     {
@@ -66,6 +66,17 @@ public class basicItemController {
         return "basic/item";
     }
 
+    @PostMapping("/add")
+    // @ModelAttribute : model 객체 생성해주기 & 괄호 안에 있는 이름으로 모델에 넣어주는 두 가지 역할을 행함
+    // 괄호 안 이름 생략하면 현재 class명 즉 Item 의 첫 글자만 소문자로 바꾼 이름을 modelattribute 에 넣어줌
+    public String addItemV3(@ModelAttribute Item item, Model model)
+    {
+        itemRepository.save(item);
+
+//        model.addAttribute("item",item); // 자동 추가가 되기 때문에 생략 가능
+
+        return "basic/item";
+    }
     /**
      *  테스트용 데이터 추가
      */
