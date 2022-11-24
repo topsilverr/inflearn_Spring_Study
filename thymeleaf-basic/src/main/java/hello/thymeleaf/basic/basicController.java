1834bf575901b0d828745bc172479a7cecd180e9
topsilverr <wltkddms9909@naver.com>
2022-11-24 17:47:12 +0900

    [feat] 유틸리티 객체와 날짜

--- thymeleaf-basic/src/main/java/hello/thymeleaf/basic/basicController.java
+++ thymeleaf-basic/src/main/java/hello/thymeleaf/basic/basicController.java
@@ -1,6 +1,7 @@
 package hello.thymeleaf.basic;
 
 import lombok.Data;
+import org.springframework.boot.Banner;
 import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -8,6 +9,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import javax.servlet.http.HttpSession;
+import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
@@ -54,6 +56,12 @@ public class basicController {
     public String basicObjects(HttpSession session){
         session.setAttribute("sessionData","Hello Session");
         return "basic/basic-objects";
+    }
+
+    @GetMapping("/date")
+    public String date(Model model){
+        model.addAttribute("localDateTime", LocalDateTime.now());
+        return "/basic/date";
     }
 
     @Component("helloBean")
