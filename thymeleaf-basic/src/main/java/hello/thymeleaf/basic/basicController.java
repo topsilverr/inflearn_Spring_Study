aad1f41362fad7fe66d7a02565d35b3880c2d605
topsilverr <wltkddms9909@naver.com>
2022-11-24 17:55:19 +0900

    [feat] URL 링크

--- thymeleaf-basic/src/main/java/hello/thymeleaf/basic/basicController.java
+++ thymeleaf-basic/src/main/java/hello/thymeleaf/basic/basicController.java
@@ -58,6 +58,14 @@ public class basicController {
         return "basic/basic-objects";
     }
 
+    @GetMapping("/link")
+    public String link(Model model){
+        model.addAttribute("param1","data1");
+        model.addAttribute("param2","data2");
+
+        return "/basic/link";
+    }
+
     @GetMapping("/date")
     public String date(Model model){
         model.addAttribute("localDateTime", LocalDateTime.now());
