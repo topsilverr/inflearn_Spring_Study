4249d7e7b46e4b6c35d2a9ef376f235477c9ed25
topsilverr <wltkddms9909@naver.com>
2022-11-24 19:16:40 +0900

    [feat] 속성 설정

--- thymeleaf-basic/src/main/java/hello/thymeleaf/basic/basicController.java
+++ thymeleaf-basic/src/main/java/hello/thymeleaf/basic/basicController.java
@@ -90,6 +90,11 @@ public class basicController {
         return "/basic/operation";
     }
 
+    @GetMapping("/attribute")
+    public String attribute(){
+        return "/basic/attribute";
+    }
+
     @Component("helloBean")
     static class HelloBean{
         public String hello(String data){
