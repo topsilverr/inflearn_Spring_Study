cf6ac7f06dcce21756766c16f44f9fc698f41ba8
topsilverr <wltkddms9909@naver.com>
2022-12-09 00:18:49 +0900

    [feat] jdbc - 조회

--- jdbc/src/test/java/hello/jdbc/repository/MemberRepositoryV0Test.java
+++ jdbc/src/test/java/hello/jdbc/repository/MemberRepositoryV0Test.java
@@ -1,18 +1,31 @@
 package hello.jdbc.repository;
 
 import hello.jdbc.domain.Member;
+import lombok.extern.slf4j.Slf4j;
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 import java.sql.SQLException;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
 
+@Slf4j
 class MemberRepositoryV0Test {
 
     MemberRepositoryV0 repository = new MemberRepositoryV0();
     @Test
     void crud() throws SQLException {
-        Member member = new Member("memberV0", 10000);
+        //save
+        Member member = new Member("memberV5", 10000);
         repository.save(member);
+        
+        //findById
+        Member findMember = repository.findById(member.getMemberId());
+        log.info("findMember={}",findMember);
+        log.info("member == findMember {}",member == findMember);
+        log.info("member equals findMember {}",member.equals(findMember));
+        // 객체 자체는 다르지만 들어있는 데이터가 같기 때문에 equal 을 사용하면 같다고 출력됨
+        assertThat(findMember).isEqualTo(member);
     }
 }
