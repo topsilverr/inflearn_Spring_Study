587e7b11e0c7fcfa87e4e3caf04781235eba1980
topsilverr <wltkddms9909@naver.com>
2022-12-28 06:39:13 +0900

    [feat] 기본 개발 환경 구축

--- japshop/src/main/java/jpabook/japshop/JapshopApplication.java
+++ japshop/src/main/java/jpabook/japshop/JapshopApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class JapshopApplication {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(JapshopApplication.class, args);
 	}
 
---
+++ japshop/src/test/java/jpabook/japshop/MemberRepositoryTest.java
@@ -0,0 +1,34 @@
+package jpabook.japshop;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Rollback;
+import org.springframework.transaction.annotation.Transactional;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+class MemberRepositoryTest {
+
+    @Autowired MemberRepository memberRepository;
+
+    @Test
+    @Transactional
+    @Rollback(false)
+    public void testMember(){
+    //given
+    Member member= new Member();
+    member.setUsername("memberA");
+    //when
+    Long saveID = memberRepository.save(member);
+    Member findMember = memberRepository.find(saveID);
+    //then
+    Assertions.assertThat(findMember.getId()).isEqualTo(member.getId());
+    Assertions.assertThat(findMember.getUsername()).isEqualTo(member.getUsername());
+    Assertions.assertThat(findMember).isEqualTo(member); // 같은 아이디라서
+    }
+
+
+}
