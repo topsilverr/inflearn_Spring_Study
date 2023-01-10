883f8e40f6dd93da9df2a775d288ada199c91b1e
topsilverr <wltkddms9909@naver.com>
2023-01-11 02:30:16 +0900

    [feat] item 엔티티에 비지니스 로직 (재고관리) 추가

--- japshop/src/main/java/jpabook/japshop/domain/item/Item.java
+++ japshop/src/main/java/jpabook/japshop/domain/item/Item.java
@@ -2,6 +2,7 @@ package jpabook.japshop.domain.item;
 
 import jakarta.persistence.*;
 import jpabook.japshop.domain.Category;
+import jpabook.japshop.exception.NotEnoughStockException;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -24,4 +25,25 @@ public abstract class Item {
 
     @ManyToMany(mappedBy = "items")
     private List<Category> categories = new ArrayList<>();
+
+    //==비지니스 로직==//
+
+    /**
+     * stock 증가
+     */
+    public void addStock(int quantity){
+        this.stockQuantity+=quantity;
+    }
+
+    /**
+     * stock 감소
+     * stock 의 수량이 0 보다 작은 경우 예외 발생
+     */
+    public void removeStock(int quantity){
+        int restStock = this.stockQuantity - quantity;
+        if(restStock<0){
+            throw new NotEnoughStockException("need more stock");
+        }
+        this.stockQuantity = restStock;
+    }
 }
