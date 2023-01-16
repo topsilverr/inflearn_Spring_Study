fbfeda8be98bfe0eea7cee80295dae9711098b5c
topsilverr <wltkddms9909@naver.com>
2023-01-17 02:26:51 +0900

    [feat] 회원저장 api Controller - 엔티티를 api 스펙에 노출하지 말자

--- jpashop/src/main/java/jpabook/jpashop/api/MemberApiController.java
+++ jpashop/src/main/java/jpabook/jpashop/api/MemberApiController.java
@@ -18,12 +18,14 @@ public class MemberApiController {
 
     @PostMapping("/api/v1/members")
     public CreateMemberResponse saveMemberV1(@RequestBody @Valid Member member){
+
         Long id = memberService.join(member);
         return new CreateMemberResponse(id);
     }
 
     @PostMapping("/api/v2/members")
     public CreateMemberResponse saveMemberV2(@RequestBody @Valid CreateMemberRequest request){
+
         Member member = new Member();
         member.setName(request.getName());
 
