924aaaa02a2fa8c615c416aac43642d822bf50d1
topsilverr <wltkddms9909@naver.com>
2023-01-27 14:03:58 +0900

    hibernate5module 추가

--- jpashop/src/main/java/jpabook/jpashop/JpashopApplication.java
+++ jpashop/src/main/java/jpabook/jpashop/JpashopApplication.java
@@ -1,5 +1,6 @@
 package jpabook.jpashop;
 
+import com.fasterxml.jackson.datatype.hibernate5.Hibernate5Module;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
@@ -11,5 +12,8 @@ public class JpashopApplication {
 		SpringApplication.run(JpashopApplication.class, args);
 	}
 
-
+	@Bean
+	Hibernate5Module hibernate5Module(){
+		return new Hibernate5Module();
+	}
 }
