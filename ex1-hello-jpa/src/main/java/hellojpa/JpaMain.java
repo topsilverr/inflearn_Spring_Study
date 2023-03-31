1cb4bc9b42745f3325c14b50623c8437fa954b1c
topsilverr <wltkddms9909@naver.com>
2023-03-31 10:55:28 +0900

    [Feat] 회원 CRUD

--- ex1-hello-jpa/src/main/java/hellojpa/JpaMain.java
+++ ex1-hello-jpa/src/main/java/hellojpa/JpaMain.java
@@ -4,6 +4,7 @@ import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.EntityTransaction;
 import javax.persistence.Persistence;
+import java.util.List;
 
 public class JpaMain {
     public static void main(String[] args) {
@@ -15,11 +16,34 @@ public class JpaMain {
         tx.begin();
 
         try {
-            Member member = new Member();
-            member.setId(2L);
-            member.setName("HelloB");
+            // 회원등록
+            //Member member = new Member();
+            //member.setId(2L);
+            //member.setName("HelloB");
+            //em.persist(member);
 
-            em.persist(member);
+            // Member 객체를 대상으로 조회
+            // m -> entity
+            // setFirstResult + setMaxResults => paging 기능
+            List<Member> result = em.createQuery("select m from Member as m", Member.class)
+                    .setFirstResult(1)
+                    .setMaxResults(10)
+                    .getResultList();
+
+            for (Member member : result) {
+                System.out.println("member.getName() = " + member.getName());
+            }
+
+            // 회원조회
+            Member findMember = em.find(Member.class, 1L);
+            // 회원수정
+            findMember.setName("HelloJPA");
+
+            //em.persist() 안 해도 됨 -> Java 컬렉션처럼 작동
+            //JPA 를 통해서 entity를 불러오면 그 순간부터 JPA 가 관리 => tx.commit 시점에 변동된 사항 있는지 없는지 확인 => 있으면 update 쿼리 자동 생성 후 실행
+
+            // 회원삭제
+            // em.remove(findMember);
 
             tx.commit();
         }catch (Exception e){
