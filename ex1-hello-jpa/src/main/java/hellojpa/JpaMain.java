5cb25e84a4668d2f266dba2c8336a9d4cec58c04
topsilverr <wltkddms9909@naver.com>
2023-04-05 13:48:39 +0900

    [Feat] 영속성 컨텍스트 1차캐시 활용해보기

--- ex1-hello-jpa/src/main/java/hellojpa/JpaMain.java
+++ ex1-hello-jpa/src/main/java/hellojpa/JpaMain.java
@@ -20,8 +20,8 @@ public class JpaMain {
 
             // 비영속 상태
             //Member member = new Member();
-            //member.setId(2L);
-            //member.setName("HelloB");
+            //member.setId(100L);
+            //member.setName("HelloJPA");
 
             // 영속상태 != DB 저장
             //em.persist(member);
@@ -30,22 +30,33 @@ public class JpaMain {
             // 삭제 => 영구적으로 삭제 요청 (DB에서도!)
             //em.remove(member);
 
+            // 1차 캐시에 저장된 회원조회
+            //Member findMember = em.find(Member.class, 100L);
+            // DB에 select 쿼리 안 날 == 1차 캐시에 저장되었음을 알 수 있음
+            //System.out.println("findMember.getId() = " + findMember.getId());
+            //System.out.println("findMember.getName() = " + findMember.getName());
+
+            // DB에 저장된 회원조회
+            Member findMember1 = em.find(Member.class, 100L); // 이때는 DB 에서 가져와야 하기 때문에 쿼리가 나가야하지만
+            Member findMember2 = em.find(Member.class, 100L); // 여기서는 1차캐시에서 가져와야 하기 때문에 쿼리가 나가면 안됨
+            // 직접 돌려보면 select 쿼리 한 번만 나가는 것 확인 가능
+
             // Member 객체를 대상으로 조회
             // m -> entity
             // setFirstResult + setMaxResults => paging 기능
-            List<Member> result = em.createQuery("select m from Member as m", Member.class)
-                    .setFirstResult(1)
-                    .setMaxResults(10)
-                    .getResultList();
+//            List<Member> result = em.createQuery("select m from Member as m", Member.class)
+//                    .setFirstResult(1)
+//                    .setMaxResults(10)
+//                    .getResultList();
+//
+//            for (Member member : result) {
+//                System.out.println("member.getName() = " + member.getName());
+//            }
 
-            for (Member member : result) {
-                System.out.println("member.getName() = " + member.getName());
-            }
 
-            // 회원조회
-            Member findMember = em.find(Member.class, 1L);
+
             // 회원수정
-            findMember.setName("HelloJPA");
+            //findMember.setName("HelloJPA");
 
             //em.persist() 안 해도 됨 -> Java 컬렉션처럼 작동
             //JPA 를 통해서 entity를 불러오면 그 순간부터 JPA 가 관리 => tx.commit 시점에 변동된 사항 있는지 없는지 확인 => 있으면 update 쿼리 자동 생성 후 실행
